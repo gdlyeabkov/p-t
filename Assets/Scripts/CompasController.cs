79398b189e475f2015807eb09e64cd4a80b02b0c
gdlyeabkov <gdlyeabkov@gmail.com>
2022-11-10 19:06:51 +0300

    add_half_compas

---
+++ Assets/Scripts/CompasController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompasController : MonoBehaviour
+{
+
+    public GameManager gameManager;
+
+    void Update()
+    {
+        if (gameManager.isInit)
+        {
+            // transform.Rotate(new Vector3(0f, gameManager.localPirate.transform.localEulerAngles.y, 0f));
+            // transform.RotateAround(gameManager.islandSphereTransform.position, Vector3.up, gameManager.rotationJoystick.Horizontal);
+            float mouseXDelta = gameManager.rotationJoystick.Horizontal / 5;
+            float yawDelta = 10f * mouseXDelta;
+            transform.RotateAround(gameManager.islandSphereTransform.position, Vector3.up, yawDelta);
+        }
+    }
+}
